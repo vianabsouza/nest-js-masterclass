9e7b572d1f28398ec2e196e8e85cd2878d8aecbc
Barbara Viana <vianabsouza@gmail.com>
2024-09-14 19:10:09 -0300

    feat: import db module in app and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, DbModule],
   controllers: [],
   providers: [],
 })

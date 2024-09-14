61cfcf50c959d16f6bb67c467a0d690e9c2be0c7
Barbara Viana <vianabsouza@gmail.com>
2024-09-14 19:44:53 -0300

    refactor: implement repository in user controller

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { Usuario } from './usuario.entity';
+import { UsuarioRepository } from './usuario.repository';
 
 @Controller('usuario')
 export class UsuarioController {
+  constructor(private repo: UsuarioRepository) {}
 
   @Post()
-  async create(@Body() usuario: Usuario) {
-    return usuario;
+  async create(@Body() user: Usuario) {
+    const newUser = await this.repo.create(user);
+    return newUser;
   }
 }
